b0653ad222094459f37f8af6fddaaf7f69fa7e49
Nuthalakanti Dinesh Dhanraj <dineshdinu492@gmail.com>
2022-06-01 14:15:39 +0530

    Read & write using Pipe

--- Nodejs_Programmes.js
+++ Nodejs_Programmes.js
@@ -1,30 +1,9 @@
-var fs=require('fs')
-var data='';
-var readerStream=fs.createReadStream('Sample.txt')
-
-//For Reader -> DATA, END, ERROR (3 TOTAL)
-readerStream.setEncoding('UTF8')
-readerStream.on('data',function(chunk){
-    data+=chunk;
-    console.log(chunk);
-})
-readerStream.on('end',function(){console.log(data)})
-readerStream.on('error',function(err){
-    console.log(err.stack);
-})
-console.log('Program Ended')
-
-
-
-
-var writerStream=fs.createWriteStream('Sample.txt')
-//For wRITE -> fINISH, ERROR (2 TOTAL)
-writerStream.write(data,'UTF8')
-writerStream.on('finish',function(){
-    console.log('COMPLETED')
-})
-writerStream.on('error',function(err){
-console.log(err.stack)
-})
-console.log('Writer program ended')
-
+var fs = require("fs");  
+// Create a readable stream  
+var readerStream = fs.createReadStream('Sample.txt');  
+// Create a writable stream  
+var writerStream = fs.createWriteStream('Sample2.txt');  
+// Pipe the read and write operations  
+// read input.txt and write data to output.txt  
+readerStream.pipe(writerStream);  
+console.log("Program Ended");   
