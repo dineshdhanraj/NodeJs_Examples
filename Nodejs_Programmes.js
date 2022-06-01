8135259e29f2d0f4935d82d9183f340d45cbb057
Nuthalakanti Dinesh Dhanraj <dineshdinu492@gmail.com>
2022-06-01 12:08:04 +0530

    Delete File 7th Program

--- Nodejs_Programmes.js
+++ Nodejs_Programmes.js
@@ -1,9 +1,7 @@
 var http=require('http')
 var fs=require('fs');
-http.createServer(function(req,res){
-    fs.open('Sample2.txt','a',function(err){
+
+    fs.unlink('Sample2.txt',function(err){
        if(err) throw err;
        console.log('Saved!');
     })
-       
-}).listen(8080);
