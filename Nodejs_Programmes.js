3eec4726061aaa101484c2f031093aa93c8d6c0c
Nuthalakanti Dinesh Dhanraj <dineshdinu492@gmail.com>
2022-06-01 13:54:53 +0530

    clearTimeout & clearImmediate

--- Nodejs_Programmes.js
+++ Nodejs_Programmes.js
@@ -1,3 +1,7 @@
-var x=setInterval(function(){console.log('I AM FROM INTERVAL')},1000) //WASTE BY GIVING TIME
+var x=setTimeout(function(){console.log('I AM FROM TIMEOUT')},1000) 
+clearTimeout(x);
 
-clearInterval(x);
+console.log('I AM PRINING')
+
+var x1=setImmediate(function(){console.log('I AM FROM IMMEDIATE')},1000) 
+clearImmediate(x1);
