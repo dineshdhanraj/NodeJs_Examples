68b054f60e722afd9a667cc676e1ca577e4f5d32
Nuthalakanti Dinesh Dhanraj <dineshdinu492@gmail.com>
2022-06-01 14:14:02 +0530

    Read & write stream partial

--- Nodejs_Programmes.js
+++ Nodejs_Programmes.js
@@ -1,7 +1,30 @@
-var x=setTimeout(function(){console.log('I AM FROM TIMEOUT')},1000) 
-clearTimeout(x);
+var fs=require('fs')
+var data='';
+var readerStream=fs.createReadStream('Sample.txt')
 
-console.log('I AM PRINING')
+//For Reader -> DATA, END, ERROR (3 TOTAL)
+readerStream.setEncoding('UTF8')
+readerStream.on('data',function(chunk){
+    data+=chunk;
+    console.log(chunk);
+})
+readerStream.on('end',function(){console.log(data)})
+readerStream.on('error',function(err){
+    console.log(err.stack);
+})
+console.log('Program Ended')
 
-var x1=setImmediate(function(){console.log('I AM FROM IMMEDIATE')},1000) 
-clearImmediate(x1);
+
+
+
+var writerStream=fs.createWriteStream('Sample.txt')
+//For wRITE -> fINISH, ERROR (2 TOTAL)
+writerStream.write(data,'UTF8')
+writerStream.on('finish',function(){
+    console.log('COMPLETED')
+})
+writerStream.on('error',function(err){
+console.log(err.stack)
+})
+console.log('Writer program ended')
+
